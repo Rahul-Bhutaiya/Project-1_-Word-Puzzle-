f5ad25c44bd1ab2334c43089d1e87025d92985e7
Rahul Bhutaiya <111126404+Rahul-Bhutaiya@users.noreply.github.com>
2022-12-26 18:00:17 +0530

    Update word_puzzle_game.py

--- word_puzzle_game.py
+++ word_puzzle_game.py
@@ -1,28 +1,36 @@
 from random import sample
 
-def update_words():
+word_list=['father','break','country','green','aeroplane','india','creative','power','computer','python','website','koo','neuralink','english','vaccine','engine','advocate','ancient','beyond','campaign','champion','circle','citizen']
+
+def update_words():#To Update List of Words...
     global word_list
     word_list=[x for x in input('Enter Any Five Words Separated By Comma : ').split(',')]
 
+def word_puzzle():
+    print("Hey! User Let's Start The Game, You Have To Arrange The Letters to Form a Valid Word...😜")
+    input('Press Any Key To Start...')
+    count,score=0,0
+    for word in sample(word_list,5):
 
-def puzzle():
-    score=0
-    print('Hey User! ,Let\'s Start The Puzzle....\n')
-    for word in set(word_list):
-        print('Arrange the Letters to Form a Valid Word :\n',''.join(sample(word,len(word))).upper(),sep='')
-        user_input=input('Enter Arranged Word : ')
-        if user_input.lower()==word.lower():
+        count+=1
+        print('\n%d. %s'%(count,''.join(sample(word,len(word)))))
+        u_word=input('Enter Arranged Word : ')
+
+        if u_word.lower()==word.lower():
+            print('Correct')
             score+=1
-            print('\nCorrect\n')
+            print('Current Score :',score)
         else:
+            print('Wrong, Correct Arrangement is :',word)
             score-=1
-            print('\nWrong\n')
+            print('Current Score :',score)
     else:
-        print('Net Score is',score)
+        print('\nFinal Score :',score)
+        if score==5:
+            print('Great Effort')
+        elif score==4:
+            print('Good Efford')
+        else:
+            print('Practice Makes Man Perfect')
 
-word_list=['father','break','country','green','aeroplane']
-
-#To Update Word List...
-#update_words() 
-
-puzzle()
+word_puzzle()
